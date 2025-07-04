commit 8: generated experimental data with 4 deg of freedom, corrected parallel computation

intensity_analysis_four.py
@@ -2,9 +2,24 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import glob
+import seaborn as sns
+import plotly.express as px
 
 # read in the csv file that we will do analysis with; file with 0.05 fft is the one thats correct
-intensities = np.load("C://Users//Nathan Cao//OneDrive//Desktop//quadoa projects//intensity_data//intensities_geo_0_2_fourdeg1.npy")
+experiment_num = 1
+filepath = "C://Users//Nathan Cao//OneDrive//Desktop//quadoa_projects//intensity_data//four_deg//experiment_" + str(experiment_num)
+
+# find all of the csv files using the path that we defined
+csv_files = sorted(glob.glob(filepath + "//*.csv"))  # e.g., "*.csv"
+
+# load the csv files and concatenate all of them
+df_list = [pd.read_csv(f) for f in csv_files]
+df_all = pd.concat(df_list, ignore_index=True)
+
+# convert the dataframe to a numpy matrix
+intensities = df_all.to_numpy()
+
 
 # variables that are used later
 yaw1_start = 43; yaw1_end = 47; yaw2_start = 133; yaw2_end = 137
@@ -17,17 +32,11 @@ yaw2 = np.arange(yaw2_start, yaw2_end, step_size)
 pitch1 = np.arange(pitch1_start, pitch1_end, step_size)
 pitch2 = np.arange(pitch2_start, pitch2_end, step_size)
 
+'''
+# ask for whether we want to analyze 2d or 3d cross section
+dimension = input("Whether to analyze twod or threed: ").strip()
 
-# ask for whether we want to analyze the pitch or yaw cross section
-axis = input("Whether to analyze pitch or yaw: ").strip()
-
-intensities = intensities[:8, :, :, :]
-intensities = intensities[:, :8, :, :]
-intensities = intensities[:, :, :8, :]
-intensities = intensities[:, :, :, :8]
-
-
-if axis == "pitch":
+if dimension == "twod":
     x1, x2 = np.meshgrid(yaw1, yaw2)
     print(x1.shape)
 
@@ -80,3 +89,25 @@ if axis == "pitch":
     # Show the plot
     plt.tight_layout()
     plt.show()
+
+if dimension == "threed":
+    # Suppose you want to select data where x4 == 0.5
+    x4_value = 0
+    
+    # Use np.isclose if x4 is a float and might have rounding issues
+    subset_df = df_all[np.isclose(df_all['pitch2'], x4_value)]
+    
+    # Plot using plotly.express
+    fig = px.scatter_3d(df_all, x='yaw1', y='yaw2', z='pitch1',
+                        color='intensity',
+                        color_continuous_scale='Viridis',
+                        title='4D Plot: x1, x2, x3 with intensity color')
+    
+    fig.update_layout(scene=dict(
+        xaxis_title='x1',
+        yaxis_title='x2',
+        zaxis_title='x3'
+    ))
+    
+    fig.show()
+'''

psf_nested.py
@@ -74,7 +74,7 @@ while yaw1 < yaw1_end:
                 core.setOpticalSystemParamByIndexD(9, 11, pitch2)
 
                 # getGeoPSF, as opposed to get FFT
-                data = core.getGeoPSF(0, 0, 0, 100)
+                data = core.getGeoPSF(0, 0, 0, 50)
 
                 # this is just the numpy array version of the PSF
                 npdata = np.array(data, copy=False)
@@ -87,7 +87,7 @@ while yaw1 < yaw1_end:
 
                 index4 += 1
                 pitch2 += step_size
-                print("Pitch2: " + str(pitch2) + " Pitch1: " + str(pitch1) + " Yaw2: " + str(yaw2) + " Yaw1: " + str(yaw1))
+                print("Pitch2: " + str(pitch2) + " Pitch1: " + str(pitch1) + " Yaw2: " + str(yaw2) + " Yaw1: " + str(yaw1) + " Power: " + str(nppower) + "\n time: " + str(time.ctime()))
             index3 += 1
             pitch1 += step_size
             index4 = 0
@@ -99,8 +99,6 @@ while yaw1 < yaw1_end:
     index1 += 1
     yaw1 += step_size
     index2 = 0
-
-    print(str(yaw1))
 
 # normalize the intensity values because too big
 intensities_normalized = intensities / np.max(intensities)

psf_parallel.py
@@ -7,6 +7,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from joblib import Parallel, delayed
 import csv
+import os
 
 '''
 # Get info about the sequence: the sequence number, the number of fields, wavelengths, etc.
@@ -21,7 +22,7 @@ fig, axs = plt.subplots(max_wave, max_field, squeeze=False)
 
 # Set the necessary parameter values
 yaw1_start = 43; yaw1_end = 47; yaw2_start = 133; yaw2_end = 137
-pitch1_start = -2; pitch1_end = 2; pitch2_start = -2; pitch2_end = 2; step_size = 0.05
+pitch1_start = -2; pitch1_end = 2; pitch2_start = -2; pitch2_end = 2; step_size = 0.1
 
 # Generate the values that we are going to use
 yaw1s = np.arange(yaw1_start, yaw1_end, step_size)
@@ -51,7 +52,7 @@ def run_simulation(yaw1, yaw2, pitch1, pitch2):
     core.setOpticalSystemParamByIndexD(9, 11, pitch2)
 
     # getGeoPSF, as opposed to get FFT
-    data = core.getGeoPSF(0, 0, 0, 100)
+    data = core.getGeoPSF(0, 0, 0, 17)
 
     # this is just the numpy array version of the PSF
     npdata = np.array(data, copy=False)
@@ -62,19 +63,28 @@ def run_simulation(yaw1, yaw2, pitch1, pitch2):
     return yaw1, yaw2, pitch1, pitch2, nppower
 
 # Do the parallel computation of the simulations
-Results = Parallel(n_jobs=-1, verbose=100)(
-    delayed(run_simulation)(i, j, k, l)
-    for i in yaw1s for j in yaw2s for k in pitch1s for l in pitch2s
-)
 
-# Define the parameters for how we want to set the filename and filepath
-simulation_num = 3
-filename = "geometric_fourdeg_" + str(simulation_num) + ".csv"
-filepath = "C://Users//Nathan Cao//OneDrive//Desktop//quadoa_projects//intensity_data//four_deg//"
-cleaned_results = [tuple(row) if hasattr(row, '__iter__') and not isinstance(row, str) else (row,) for row in Results]
+yaw1s_split = np.array_split(yaw1s, 10)
+print(yaw1s_split)
 
-# Generate the actual csv file
-with open(filepath + filename, 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(['yaw1', 'yaw2', 'pitch1', 'pitch2', 'intensity'])  # header
-    writer.writerows(cleaned_results)
+file_num = 1
+
+for arr in yaw1s_split:
+
+    Results = Parallel(n_jobs = 40, verbose = 100)(
+        delayed(run_simulation)(i, j, k, l)
+        for i in arr for j in yaw2s for k in pitch1s for l in pitch2s
+    )
+
+    # Define the parameters for how we want to set the filename and filepath
+    filename = "//geometric_fourdeg_" + str(file_num) + ".csv"
+    filepath = "C://Users//Nathan Cao//OneDrive//Desktop//quadoa_projects//intensity_data//four_deg//experiment_one"
+    cleaned_results = [tuple(row) if hasattr(row, '__iter__') and not isinstance(row, str) else (row,) for row in Results]
+
+    # Generate the actual csv file
+    with open(filepath + filename, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['yaw1', 'yaw2', 'pitch1', 'pitch2', 'intensity'])  # header
+        writer.writerows(cleaned_results)
+
+    file_num += 1